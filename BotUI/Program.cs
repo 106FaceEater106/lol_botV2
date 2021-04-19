6de8da27f656b719ade471032f649ced337fe615
Tobias Lisbakken <tobman@live.no>
2021-04-19 17:03:09 +0200

    remove dmg console

--- BotUI/Program.cs
+++ BotUI/Program.cs
@@ -7,17 +7,11 @@ using System.Windows.Forms;
 
 namespace BotUI {
     static class Program {
-
-        [DllImport("kernel32.dll")]
-        static extern bool AllocConsole();
-
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
-            AllocConsole();
-            Console.ReadKey();
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
